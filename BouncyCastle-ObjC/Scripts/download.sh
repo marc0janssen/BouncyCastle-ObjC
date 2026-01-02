15e3adc7e2761cd375225ccabbdd40ac4bf44f71
Marco <marco@mjanssen.nl>
2026-01-02 17:06:17 +0100

    Build J2ObjC from source tag 3.1

--- BouncyCastle-ObjC/Scripts/download.sh
+++ BouncyCastle-ObjC/Scripts/download.sh
@@ -2,26 +2,26 @@
 set -ev
 
 J2OBJC_VERSION="3.1"
+J2OBJC_SOURCE_URL="https://github.com/google/j2objc/archive/refs/tags/${J2OBJC_VERSION}.zip"
+J2OBJC_ARCHIVE="j2objc-${J2OBJC_VERSION}.zip"
 
 if [[ -d dist ]]; then
   exit
 fi
 
-echo "Fetching J2ObjC v${J2OBJC_VERSION} from https://github.com/google/j2objc/releases/download/${J2OBJC_VERSION}/j2objc-${J2OBJC_VERSION}.zip"
-curl -OL https://github.com/google/j2objc/releases/download/${J2OBJC_VERSION}/j2objc-${J2OBJC_VERSION}.zip
-# echo "${SHA1_CHECKSUM}  j2objc-${J2OBJC_VERSION}.zip" | shasum -c
-unzip -o -q j2objc-${J2OBJC_VERSION}.zip
+echo "Fetching J2ObjC v${J2OBJC_VERSION} sources from ${J2OBJC_SOURCE_URL}"
+curl -L -o ${J2OBJC_ARCHIVE} ${J2OBJC_SOURCE_URL}
+unzip -o -q ${J2OBJC_ARCHIVE}
 
 J2OBJC_PATH=j2objc-${J2OBJC_VERSION}
 
+pushd ${J2OBJC_PATH}
+make dist
+popd
+
 mkdir dist
-mv  $J2OBJC_PATH/include \
-    $J2OBJC_PATH/lib \
-    $J2OBJC_PATH/frameworks \
-    $J2OBJC_PATH/j2objc \
-    $J2OBJC_PATH/j2objcc \
-    dist
-rm -rf j2objc-${J2OBJC_VERSION} j2objc-${J2OBJC_VERSION}.zip
+mv  $J2OBJC_PATH/dist/* dist
+rm -rf ${J2OBJC_PATH} ${J2OBJC_ARCHIVE}
 
 touch dist/VERSION
 echo $J2OBJC_VERSION > dist/VERSION
